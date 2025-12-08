c84f628608968e6e1f4a90499b7acc929562b7c6
Ibraheem Farrukh <ibraheemfarrukh-study@gmail.com>
2025-12-08 19:49:16 +0500

    updated server and client

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("Bhaago Server Running")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const cors = require('cors');
+const corsOptions = {origin: 'http://localhost:5173'};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
